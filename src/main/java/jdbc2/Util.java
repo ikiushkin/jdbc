54330588085990a70ebc91b9fbb7458eb5a8252b
Igor Kiushkin <ivk230595@gmail.com>
2021-08-22 23:58:12 +0300

    add comments

--- src/main/java/jdbc2/Util.java
+++ src/main/java/jdbc2/Util.java
@@ -12,18 +12,19 @@ public class Util {
 
     static void copyDatabase(String jdbcPropertyFile, String DBName) {
         try(Connection connection = getConnection(jdbcPropertyFile)) {
+            // Копируем структуру исходной БД в новую и получаем название копии
             String to = copyDatabaseStructure(jdbcPropertyFile, DBName);
 
+            // Получаем список таблиц исходной БД
             PreparedStatement ps = connection.prepareStatement("SHOW TABLES;");
-
             List<String> tables = new ArrayList<>();
-
             ResultSet resultSet = ps.executeQuery();
 
             while (resultSet.next()) {
                 tables.add(resultSet.getString(1));
             }
 
+            // Копируем содержимое таблиц в новую БД
             for (String table : tables) {
                 String SQL = String.format("INSERT INTO %s.%s SELECT * FROM %s.%s;", to, table, DBName, table);
                 ps = connection.prepareStatement(SQL);
@@ -42,7 +43,7 @@ public class Util {
 
         try(Connection connection = getConnection(jdbcPropertyFile)) {
             PreparedStatement ps = connection.prepareStatement("SHOW TABLES;");
-
+            // Лист с таблицами исходной БД
             List<String> tables = new ArrayList<>();
 
             ResultSet resultSet = ps.executeQuery();
@@ -50,17 +51,18 @@ public class Util {
             while (resultSet.next()) {
                 tables.add(resultSet.getString(1));
             }
-
+            // Создаем копию БД
             ps = connection.prepareStatement("DROP DATABASE IF EXISTS " + newBDName);
             ps.executeUpdate();
-
             ps = connection.prepareStatement("CREATE DATABASE " + newBDName);
             ps.executeUpdate();
 
             ps = connection.prepareStatement("USE " + newBDName);
             ps.executeUpdate();
 
+            // Формируем select для создания поочередно каждой таблицы
             for (String tablesName : tables) {
+                // Читаем названия всех колонок каждой таблицы
                 ps = connection.prepareStatement("SHOW COLUMNS FROM " + DBName + "." + tablesName);
 
                 ResultSet rs = ps.executeQuery();
@@ -72,11 +74,12 @@ public class Util {
                         .append(tablesName)
                         .append(" (\n");
 
+                // Читаем построчно каждую колонку исходной таблицы и формируем из неё новый запрос для добавления
                 while (rs.next()) {
                     StringBuilder sb = new StringBuilder();
                     for (int j = 1; j < rsmd.getColumnCount() + 1; j++) {
                         String columnType = rsmd.getColumnName(j);
-
+                        // Обработка CONSTRAINT
                         switch (columnType) {
                             case "Null":
                                 if (rs.getString(j).equals("NO")) {
@@ -103,7 +106,7 @@ public class Util {
                         }
                         sb.append(rs.getString(j)).append(" ");
                     }
-
+                    // PRIMARY KEY для ID
                     if (sb.toString().startsWith("ID")) {
                         sb.append("PRIMARY KEY").append(" ");
                     }
